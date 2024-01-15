8e84061ee4bdf8b6b56c3a88c4da2072a75a42d3
Sundaram Singh <sundaramsinghsdnr@gmail.com>
2024-01-15 12:17:42 +0530

    Lazy landing guide

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import './App.css'
+import { Suspense, lazy } from 'react'
+const Dashboard = lazy(() => import('./components/Dashboard'))
+const Landing = lazy(() => import('./components/Landing'))
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+    
+        <Appbar />
+
+        <Routes>
+          <Route path="/" element={
+            <Suspense fallback={"loading..."}> 
+              <Landing /> 
+            </Suspense>} />
+
+          <Route path="/dashboard" element={
+          <Suspense fallback={"loading..."}> 
+            <Dashboard /> 
+          </Suspense>} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  
+  return <div>
+    <button onClick={() => {navigate("/")}}> Landing </button>
+    <button onClick={() => {navigate("/dashboard")}}> Dashboard </button>
+  </div>
+}
+
+export default App
